66b8efd7ea82faef0ff818efaef8672611ce0d9e
andrewni <ajni@ucdavis.edu>
2018-01-09 15:15:25 -0800

    js functionality start

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 console.log('test');
+
+document.getElementById('loan-form').addEventListener('submit', calculateResults);
+
+function calculateResults(event) {
+
+  const loanAmount = document.getElementById('amount');
+  const interest = document.getElementById('interest');
+  const years = document.getElementById('years');
+  const monthlyPayment = document.getElementById('monthly-payment');
+  const totalPayment = document.getElementById('total-payment');
+  const totalInterest = document.getElementById('total-interest');
+
+  event.preventDefault();
+}
