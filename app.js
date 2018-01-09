03b21ad25dc3299021676fd2902867c0ebb901e3
andrewni <ajni@ucdavis.edu>
2018-01-09 15:21:36 -0800

    added some calculations

--- app.js
+++ app.js
@@ -4,12 +4,26 @@ document.getElementById('loan-form').addEventListener('submit', calculateResults
 
 function calculateResults(event) {
 
-  const loanAmount = document.getElementById('amount');
-  const interest = document.getElementById('interest');
-  const years = document.getElementById('years');
-  const monthlyPayment = document.getElementById('monthly-payment');
-  const totalPayment = document.getElementById('total-payment');
-  const totalInterest = document.getElementById('total-interest');
+  const UIloanAmount = document.getElementById('amount');
+  const UIinterest = document.getElementById('interest');
+  const UIyears = document.getElementById('years');
+  const UImonthlyPayment = document.getElementById('monthly-payment');
+  const UItotalPayment = document.getElementById('total-payment');
+  const UItotalInterest = document.getElementById('total-interest');
+
+  const principal = parseFloat(UIloanAmount.value);
+  const calculatedInterest = parseFloat(UIinterest.value) / 100 / 12;
+  const calculatedPayments = parseFloat(UIyears.value) * 12;
+
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthly = (principal * x * calculatedInterest) / (x - 1);
+  
+  if (isFinite(monthly)) {
+    UImonthlyPayment.value = monthly.toFixed(2);
+    UItotalPayment.value = (monthly * calculatedPayments).toFixed(2);
+  } else {
+
+  }
 
   event.preventDefault();
 }
